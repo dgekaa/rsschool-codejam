27cb836f2f879f415077bae834f1e48ebf640a75
dgekaa <dgeka_vip@mail.ru>
2018-10-28 15:37:15 +0300

    fix: changed make

--- make.js
+++ make.js
@@ -1,17 +1,19 @@
 
-function sum(x,y){
-	return x+y;
+function sum(a,b){
+	return a+b;
 }
-function make(a){
-	function inner(...arr){
-		return function (...arr1){
-			return function (sum){
-				let array = [].concat(a,arr,arr1);
-				return make(array.length>1 ? array.reduce(sum,0) : a);
-			}
-		}	
-	}
-	inner.valueOf = function(){return a};
-	return inner;
-};
-console.log(make(1)(1,1)(1,1,1)(sum))
+
+function make(...arr) {
+  const argArray = [];
+
+  const inner = (...arr1) => {
+  	if (typeof arr1[0] === 'function') {
+    	return argArray.reduce(arr1[0]);
+  	}else{
+    	argArray.push(...arr1);
+   		return inner;
+  	}
+  };
+  return inner(...arr);
+}
+console.log(make(1)(1,1)(1,1,1)(1)(sum))
